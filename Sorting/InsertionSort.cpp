f1eee54ae324f85d100c429bd5c035eedc1868ae
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-10-09 23:58:29 +0530

    adding insertionSort code

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int a[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = a[i];
+        j = i - 1;
+        // Move elements of arr[0..i-1],
+        // that are greater than key, 
+        // to one position ahead of their
+        // current position
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = key;
+    }
+}
+
+//Driver Code
+int main()
+{
+    int n; cin >> n;
+    int a[n]; 
+    for(int i = 0; n > i; i++) cin>>a[i]; 
+    
+    bubbleSort(a, n);
+    //Print the sorted array 
+    for(int i = 0; n > i; i++){
+        cout << a[i] <<" ";
+    }
+}
+//Basic intuition : Compare two Adjacent elements and swap it.
+//Time Complexity = O(N^2) [Worst and Average]. O(N) [Best - Array is already sorted]
+//Space Complexity = O(1)
