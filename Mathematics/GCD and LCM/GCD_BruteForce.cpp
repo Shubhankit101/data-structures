9dc010bf12a7f86930861866f7b66add07c66e2d
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-01-28 14:50:42 +0530

    Code refactoring

--- Mathematics/GCD and LCM/GCD_BruteForce.cpp
+++ Mathematics/GCD and LCM/GCD_BruteForce.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-  int a, b;
-  cin >> a >> b;
+int gcd(int a, int b) {
   int sum = min(a, b);
   while (sum > 0) {
     if (a % sum == 0 && b % sum == 0) {
@@ -11,5 +8,10 @@ int main() {
     }
     sum--;
   }
-  cout << sum << endl;
+  return sum;
+}
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << gcd(a, b) << endl;
 }
