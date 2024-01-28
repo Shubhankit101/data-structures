ebbc45063c22b8c6c0a11ffa45dc266c257a48c2
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-01-28 15:10:35 +0530

    adding LCM code

---
+++ Mathematics/GCD and LCM/LCM_BruteForce.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int lcm(int a, int b) {
+  int res = max(a, b);
+  while (true) {
+    if (res % a == 0 && res % b == 0)
+      break;
+    else
+      res++;
+  }
+  return res;
+}
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << lcm(a, b) << endl;
+}
