6988c4e4358eb911eb3e2af46a3ba4ee76e485bc
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-02-07 23:48:19 +0530

    Code Refacotoring

--- BitMasking/BitwiseOperators.cpp
+++ BitMasking/BitwiseOperators.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-  int a, b;
-  cout << "Enter 1st Number" << endl;
-  cin >> a;
-  cout << "Enter 2nd Number" << endl;
-  cin >> b;
-
-  cout << "Choose Operation among & (AND), | (OR), ^ (XOR)" << endl;
-  char c;
-  cin >> c;
-
+void calculate(int a, int b, char c) {
   switch (c) {
   case '&':
     cout << (a & b) << endl;
@@ -24,8 +13,23 @@ int main() {
   case '^':
     cout << (a ^ b);
     break;
+
   default:
     cout << "Invalid Operator" << endl;
     break;
   }
+}
+
+int main() {
+  int a, b;
+  cout << "Enter 1st Number" << endl;
+  cin >> a;
+  cout << "Enter 2nd Number" << endl;
+  cin >> b;
+
+  cout << "Choose Operation among & (AND), | (OR), ^ (XOR)" << endl;
+  char c;
+  cin >> c;
+  // Function to Calculate the Bitwise Operator
+  calculate(a, b, c);
 }
