03c022992579bfac09bafe8d0be6b048546728f1
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-02-23 15:05:34 +0530

    Create CountSetBits.cpp

---
+++ BitMasking/CountSetBits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// Naive approach
+int countSet(int n) {
+  int x = 1;
+  int cnt = 0;
+  while (n > 0) {
+    // Checking the left most bit
+    if ((n & x) == 1) // If 1 meaning bit was set hence we increase the count
+      cnt++;
+    n = n >> 1; // Right shifting 1 bit
+  }
+  return cnt;
+}
+int main() {
+
+  int n;
+  // Enter the number n
+  cin >> n;
+  cout << countSet(n);
+}
