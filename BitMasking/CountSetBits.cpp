f533364b8d15bc82748e16f2e7075a4de48280a1
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-02-23 15:45:18 +0530

    Adding Brian Kernighan’s algorithm

--- BitMasking/CountSetBits.cpp
+++ BitMasking/CountSetBits.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
-
-
 // Naive approach
-// Time Complexity : O(n) 
-// Space Complexity: O(1) 
-
+// Time Complexity : O(n)
+// Space Complexity: O(1)
 int countSet(int n) {
   int x = 1;
   int cnt = 0;
   while (n > 0) {
     // Checking the left most bit
-    if ((n & x) != 0) // If 1, meaning bit was set, hence we increase the count (n % 2 == 1)
+    if ((n & x) != 0) // If 1, meaning bit was set, hence we increase the count
+                      // (n % 2 == 1)
       cnt++;
     n = n >> 1; // Right shifting 1 bit, Alternatively can be used n /= 2;
+  }
+  return cnt;
+}
+// Brian Kernighan’s Algorithm
+// Time Complexity : O(x) where x is the number of Set bits
+// Space Complexity : O(1)
+int countSetOptimised(int n) {
+  int cnt = 0;
+  while (n > 0) {
+    n = n & (n - 1);
+    cnt++;
   }
   return cnt;
 }
@@ -22,5 +31,5 @@ int main() {
   int n;
   // Enter the number n
   cin >> n;
-  cout << countSet(n);
+  cout << countSetOptimised(n);
 }
