b2f67fedc8fcc08d32e09b9f03b098698c538bf8
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-02-23 15:09:50 +0530

    Code fix

--- BitMasking/CountSetBits.cpp
+++ BitMasking/CountSetBits.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 // Naive approach
+// Time Complexity : O(n) 
+// Space Complexity: O(1) 
 int countSet(int n) {
   int x = 1;
   int cnt = 0;
   while (n > 0) {
     // Checking the left most bit
-    if ((n & x) == 1) // If 1 meaning bit was set hence we increase the count
+    if ((n & x) != 0) // If not 0, meaning bit was set, hence we increase the count
       cnt++;
     n = n >> 1; // Right shifting 1 bit
   }
