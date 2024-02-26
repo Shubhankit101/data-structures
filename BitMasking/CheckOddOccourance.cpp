c6dc16fd0d1e2cae3e7ba182f17966ce8a58538e
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-02-26 14:26:34 +0530

    Efficient Approach

--- BitMasking/CheckOddOccourance.cpp
+++ BitMasking/CheckOddOccourance.cpp
@@ -19,7 +19,18 @@ int findOddOccourance(vector<int> v) {
   return 0;
 }
 
+// Efficient Approach using XOR Operator
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+int findOddOccouranceEfficiently(vector<int> &v) {
+  int n = v.size();
+  int res = v[0];
+  for (int i = 1; n > i; i++)
+    res = res ^ v[i];
+  return res;
+}
+
 int main() {
-  vector<int> v = {1, 1, 2, 2, 2, 1, 2, 7, 7, 9, 9, 9, 10, 10};
-  cout << findOddOccourance(v);
+  vector<int> v = {4, 4, 7, 4, 8, 7, 7, 7, 8};
+  cout << findOddOccouranceEfficiently(v);
 }
