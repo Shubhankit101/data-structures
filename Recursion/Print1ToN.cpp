f7461566cc1c6cde56770fb7a5c1f2354a280893
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2024-03-03 16:43:47 +0530

    Create Print1ToN.cpp

---
+++ Recursion/Print1ToN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void print1ToN(int n) {
+  // Base Condition
+  if (n == 0)
+    return;
+
+  // Recusive call
+  print1ToN(n - 1);
+  cout << n << " ";
+}
+int main() {
+  int n;
+  cin >> n;
+  print1ToN(n);
+}
