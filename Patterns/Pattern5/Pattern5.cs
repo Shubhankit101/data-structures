cbf441e6d8172ab8b77208ee3519edb0498b6a2d
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-02-21 00:47:09 +0530

    Added Pattern5 code

---
+++ Patterns/Pattern5/Pattern5.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Pattern {
+  public class Program {
+    private static void Print(int cnt) {
+      // Print the row each time
+      for (int j = 1; cnt >= j; j++) {
+        Console.Write("*");
+      }
+      Console.WriteLine();
+    }
+    public static void Main(string[] args) {
+      int n = Convert.ToInt32(Console.ReadLine());
+
+      for (int i = 1; n >= i; i++) {
+        Print(n - i + 1);
+      }
+    }
+  }
+}
