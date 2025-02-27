5183de5a01ddd2626b4cedb35c577ecfe88bda2e
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-02-27 12:26:14 +0530

    Code Refactoring2

--- Arrays/SumOfArrayElements.cpp
+++ Arrays/SumOfArrayElements.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
-using namespace std; 
 
-int sum(int arr[], int n){
-    int result = 0; 
-    // Iterate through each element and add the sum in the result
-    for(int i = 0; n > i; i++){
-        result +=  arr[i];
-    }
-    return result;
+using namespace std;
+
+int sum(int arr[], int n) {
+  //Initialise the result with 0 
+  int result = 0;
+  // Iterate through each element in the array
+  for (int i = 0; n > i; i++) {
+    // Add each element to the result
+    result += arr[i];
+  }
+  return result;
 }
 
-int main(){
+// The main Function
+int main() {
 
-    cout<<"Enter the size of the Array"<<endl;
-    int n; 
-    cin >> n;
-    cout<<"Enter Array Elements"<<endl;
-    int arr[n];
-    for(int i = 0; n > i; i++){
-        cin >> arr[i];
-    }
-    cout << "Sum of the elements is "<< sum(arr, n) << endl; 
+  cout << "Enter the size of the Array" << endl;
+  int n;
+  cin >> n;
+  cout << "Enter Array Elements" << endl;
+  int arr[n];
+  for (int i = 0; n > i; i++) {
+    cin >> arr[i];
+  }
+  cout << "Sum of the elements is " << sum(arr, n) << endl;
 
-    return 0;
+  return 0;
 }
