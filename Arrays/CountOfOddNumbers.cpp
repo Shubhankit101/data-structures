ae7a7380c27285ade94a275b70dca5de5e1092f1
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-03-03 19:55:26 +0530

    Create CountOfOddNumbers.cpp

---
+++ Arrays/CountOfOddNumbers.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int sum(int arr[], int n) {
+  //Initialise the result with 0 
+  int result = 0;
+  // Iterate through each element in the array
+  for (int i = 0; n > i; i++) {
+    if(arr[i] % 2 != 0){
+        result += 1; 
+    }
+  }
+  return result;
+}
+
+// The main Function
+int main() {
+    cout << "Enter the size of the Array" << endl;
+    int n;
+    cin >> n;
+    cout << "Enter Array Elements" << endl;
+    int arr[n];
+    for (int i = 0; n > i; i++) {
+      cin >> arr[i];
+    }
+    cout << "Number of Odd Elements are " << sum(arr, n) << endl;
+    return 0;
+  }
